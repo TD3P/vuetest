296b8b98064325bdd1766ec78292985c26c7ecf5
TD3P <tadatsugu.sampei@gmail.com>
2019-03-03 21:05:31 +0900

    色々

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+<div>
+  <form>
+    <button v-on:click="addTodo()">ADD TASK</button>
+    <button>DELETE FINISHED TASKS</button>
+    <!-- <p>input: <input type="text"></p>
+      <p>task:</p> -->
+    <p>input: <input type="text" v-model="newTodo" placeholder="edit me"></p>
+    <p>task: {{ newTodo }}</p>
+  </form>
 
-
-    <form>
-      <button>ADD TASK</button>
-      <button>DELETE FINISHED TASKS</button>
-      <p>input: <input type="text"></p>
-      <p>task:</p>
-    </form>
-
-    <div class="task-list">
-
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-router</label>
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vuex</label>
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-loader</label>
-      <label class="task-list__item--checked"><input type="checkbox" checked><button>EDIT</button>awesome-vue</label>
-    </div>
-
-
+  <div class="task-list">
+    <label class="task-list__item" v-for="todo in todos" v-bind:key="todo.text">
+      <input type="checkbox">
+      <button>EDIT</button>
+      {{todo.text}}
+    </label>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todos: [{
+        text: 'vue-router',
+        done: false
+      },
+      {
+        text: 'vuex',
+        done: false
+      },
+      {
+        text: 'vue-loader',
+        done: false
+      },
+      {
+        text: 'aホゲホゲ',
+        done: true
+      }
+      ],
+      newTodo: ''
+    }
+  },
+  method: {
+    addTodo: function (event) {
+      let text = this.newTodo && this.newTodo.trim()
+      if (!text) {
+        return
+      }
+      this.todos.push({
+        text: text,
+        done: false
+      })
+      this.newTodo = ''
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style lang="scss" scoped>
+@mixin flex-vender() {
+    display: flex;
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.task-list {
+    @include flex-vender;
+    flex-direction: column;
+    align-items: center;
+    &__item {
+        width: 270px;
+        text-align: left;
+        $element: #{&};
+        &--checked {
+            @extend #{$element};
+            color: #85a6c6;
+        }
+    }
 }
 </style>
